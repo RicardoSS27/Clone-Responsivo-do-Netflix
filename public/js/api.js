eb998469383723e1ae33a9875447bce62949017b
Ricardo Sousa <ricardosousa2709y@gmail.com>
2022-12-13 17:25:31 -0300

    movie poster

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "592ad5e058eae17fa5cfb5473592af89"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
---
+++ public/js/scroll.js
@@ -0,0 +1,4 @@
+const SetupScrolling = () =>{
+    const container = [...document.querySelectorAll(.movie_container)]
+    const nextBtn = [...document.querySelectorAll(.movie_container)]
+}
